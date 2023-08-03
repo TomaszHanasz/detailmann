90bc4b011d99aab8a3068af3ead4959b7a7e88ea
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-08-03 09:19:06 -0500

    gallery title added

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -27,50 +27,57 @@ const Gallery = () => {
   return (
     <>
       <Fade>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          speed={2000}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          autoplay={{
-            delay: 4000,
-            disableOnInteraction: false,
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation, Autoplay]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img className="swiper__img" src={image1} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image2} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image6} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image4} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image5} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image3} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image7} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image8} alt="zdjęcie" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="swiper__img" src={image9} alt="zdjęcie" />
-          </SwiperSlide>
-        </Swiper>
+        <div className="gallery__container">
+          <h2 className="gallery__title">
+            Świadczymy usługi na terenie Poznania i okolic 7 dni w tygodniu
+          </h2>
+          <br />
+          <br />
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            speed={2000}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation, Autoplay]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img className="swiper__img" src={image1} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image2} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image6} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image4} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image5} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image3} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image7} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image8} alt="zdjęcie" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper__img" src={image9} alt="zdjęcie" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </Fade>
     </>
   );
