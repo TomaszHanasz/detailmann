e405c4cdc7872efba8c096aaea1d772a849e4ee1
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-08-04 10:18:00 -0500

    style update

--- src/components/offerBlock/OfferBlock.jsx
+++ src/components/offerBlock/OfferBlock.jsx
@@ -9,27 +9,30 @@ import "./offerBlock.style.css";
 const OfferBlock = () => {
   return (
     <div>
-      <Fade>
-        <h2 className="offer-block__title fancy">
-          Dlaczego powinieneś wybrać nas
-        </h2>
-      </Fade>
+      <h2 className="offer-block__title fancy">
+        Dlaczego powinieneś wybrać nas
+      </h2>
       <Fade cascade>
         <div className="offer-block__content">
           <div className="offer-block">
-            <div className="offer-block__overlay"></div>
-            <img src={blokOne} className="offer-block__image" alt="wygoda" />
-            <div className="offer-block__text">
-              <h3>Wygoda</h3>
-              <br />
+            <div className="offer-block__overlay">
               <p>
                 Tradycyjne myjnie samochodowe i sklepy detaliczne wymagają czasu
                 i oczekiwania. My przyjeżdżamy do Ciebie.
               </p>
             </div>
+            <img src={blokOne} className="offer-block__image" alt="wygoda" />
+            <div className="offer-block__text">
+              <h3>Wygoda</h3>
+            </div>
           </div>
           <div className="offer-block">
-            <div className="offer-block__overlay"></div>
+            <div className="offer-block__overlay">
+              <p>
+                Niezależnie od tego, czy jesteś w domu, czy w ulubionym sklepie,
+                rób to, co lubisz, podczas gdy my zajmiemy się obowiązkiem.
+              </p>
+            </div>
             <img
               src={blokTwo}
               className="offer-block__image"
@@ -37,15 +40,15 @@ const OfferBlock = () => {
             />
             <div className="offer-block__text">
               <h3>Oszczędność czasu</h3>
-              <br />
-              <p>
-                Niezależnie od tego, czy jesteś w domu, czy w ulubionym sklepie,
-                rób to, co lubisz, podczas gdy my zajmiemy się obowiązkiem.
-              </p>
             </div>
           </div>
           <div className="offer-block">
-            <div className="offer-block__overlay"></div>
+            <div className="offer-block__overlay">
+              <p>
+                Poświęcając jak najmniej jakości, znaleźliśmy sposoby na
+                zaoszczędzenie i przekazanie tych oszczędności Tobie.
+              </p>
+            </div>
             <img
               src={blokThree}
               className="offer-block__image"
@@ -53,23 +56,18 @@ const OfferBlock = () => {
             />
             <div className="offer-block__text">
               <h3>Oszczędność pieniędzy</h3>
-              <br />
-              <p>
-                Poświęcając jak najmniej jakości, znaleźliśmy sposoby na
-                zaoszczędzenie i przekazanie tych oszczędności Tobie.
-              </p>
             </div>
           </div>
           <div className="offer-block">
-            <div className="offer-block__overlay"></div>
-            <img src={blokFour} className="offer-block__image" alt="jakość" />
-            <div className="offer-block__text">
-              <h3>Jakość</h3>
-              <br />
+            <div className="offer-block__overlay">
               <p>
                 Wszystko, od dywanika i siedzeń po lakier nadwozia i koła,
                 będzie wyglądać „jak nowe”.
               </p>
+            </div>
+            <img src={blokFour} className="offer-block__image" alt="jakość" />
+            <div className="offer-block__text">
+              <h3>Jakość</h3>
             </div>
           </div>
         </div>
